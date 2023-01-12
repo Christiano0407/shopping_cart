91caefc65b2fb730dc4d939b0385b07c256db4d5
Christiano0407 <placebo0704@gmail.com>
2023-01-12 13:22:13 -0600

    add new menu icons and responsive

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //**TODO === ====================  ===  Shopping Cart === ===========================  === */
+const btnMenu = document.querySelector(`#idBtnMenu`);
+const navMobile = document.querySelector(`#navMobile`);
+//** === Events */
+
+btnMenu.addEventListener(`click`, () => {
+  console.log('Click');
+
+  if (navMobile) {
+    navMobile.classList.toggle('hidden');
+  }
+});
