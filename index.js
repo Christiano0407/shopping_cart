a91b5ae5588992bd91deb2622f2972fe8100c717
Christiano0407 <placebo0704@gmail.com>
2023-01-13 12:56:35 -0600

    git push

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 //**TODO === ====================  ===  Shopping Cart === ===========================  === */
 const btnMenu = document.querySelector(`#idBtnMenu`);
 const navMobile = document.querySelector(`#navMobile`);
+const cardAmount = document.querySelector(`.card-amount`);
+const btnShop = document.querySelector(`#idBtnShop`);
 //** === Events */
 
 btnMenu.addEventListener(`click`, () => {
-  console.log('Click');
+  console.log('menu');
 
   if (navMobile) {
     navMobile.classList.toggle('hidden');
   }
+});
+
+btnShop.addEventListener(`click`, () => {
+  console.log('Shop Card');
 });
