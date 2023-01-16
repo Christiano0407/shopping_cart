290a2526065a8292a0e046dfb8b00d17bd098c9f
Christiano0407 <placebo0704@gmail.com>
2023-01-16 12:13:53 -0600

    increment && decrement

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-//**TODO === ====================  ===  Shopping Cart === ===========================  === */
+//**TODO === ====================  === === === === ===  Shopping Cart === === === === ===========================  === */
 const btnMenu = document.querySelector(`#idBtnMenu`);
 const navMobile = document.querySelector(`#navMobile`);
 //**! === Add Shopping Cart */
 const btnShop = document.querySelector(`#idBtnShop`);
 const cardAmount = document.querySelector(`.card-amount`);
-const quantity = document.querySelector(`.quantity`);
-const btnMinus = document.querySelector(`.btn-minus`);
-const btnPlus = document.querySelector(`btn-plus`);
+
+//const btnMinus = document.querySelector(`.btn-minus`);
+//const btnPlus = document.querySelector(`btn-plus`);
 //**! === Add Card With Template Literals */
 let cardShop = document.getElementById(`cardShop`);
 let clothesShopData = [
   {
-    id: 1,
+    id: `clothesOne`,
     name: `T-Shirt Men`,
     price: 500,
     text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -20,7 +20,7 @@ let clothesShopData = [
     img: `/src/img/shirtImg/shirt.jpg`,
   },
   {
-    id: 2,
+    id: `clothesTwo`,
     name: `T-Shirt Edition Special`,
     price: 350,
     text: ` Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -29,7 +29,7 @@ let clothesShopData = [
     img: `/src/img/shirtImg/shirt1.jpg`,
   },
   {
-    id: 3,
+    id: `clothesThird`,
     name: `T-Shirt White Snow`,
     price: 250,
     text: `    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -38,7 +38,7 @@ let clothesShopData = [
     img: `/src/img/shirtImg/shirt2.jpg`,
   },
   {
-    id: 4,
+    id: `clothesFour`,
     name: `T-Shirt Special line`,
     price: 450,
     text: `    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -52,6 +52,7 @@ let clothesShopData = [
 let counter = 0;
 
 //** ====  === Events === ==== */
+//** === Menu */
 btnMenu.addEventListener(`click`, () => {
   //console.log('menu');
 
@@ -59,16 +60,22 @@ btnMenu.addEventListener(`click`, () => {
     navMobile.classList.toggle('hidden');
   }
 });
-
+//** === Shop Card */
 btnShop.addEventListener(`click`, () => {
   console.log('Shop Card');
 });
 
+//** === Basket */
+let basket = [];
+//console.log(basket);
+
+//** === Shop && Custom Element */
 const generateShop = () => {
   return (cardShop.innerHTML = clothesShopData
     .map((item) => {
+      let { id, name, price, text, img } = item;
       return `
-     <div class="card">
+     <div id=cardProductId-${id}  class="card">
         <div class="card-title">
           <h1 class="card-tittle__clothes">Clothes New Shirt</h1>
         </div>
@@ -77,25 +84,25 @@ const generateShop = () => {
           <figure class="card-img__figure">
             <img
               class="card-img__image"
-              src="${item.img}"
+              src="${img}"
               alt="shirt-img"
             />
           </figure>
         </div>
         <div class="card-content">
-          <h2 class="card-content__title">${item.name}</h2>
+          <h2 class="card-content__title">${name}</h2>
           <p class="card-content__text">
-            ${item.text}
+            ${text}
           </p>
         </div>
         <div class="card-plus">
           <span class="card-plus__cash">${item.price}</span>
           <div class="card-plus__more">
-            <button class="btn-minus">
+            <button onclick="decrement(${id})"  class="btn-minus">
               <i class="fa-solid fa-minus"></i>
             </button>
-            <span id="idQuantity" class="quantity">0</span>
-            <button class="btn-plus">
+            <span id=${id} class="quantity">0</span>
+            <button onclick="increment(${id})"  class="btn-plus">
               <i class="fa-solid fa-plus"></i>
             </button>
           </div>
@@ -108,3 +115,37 @@ const generateShop = () => {
 };
 
 generateShop();
+
+//** === Increment or Decrement && Update  ===  */
+
+const decrement = (id) => {
+  let selectedID = id;
+  //console.log(selectedID);
+
+  let search = basket.find((item) => item.id === selectedID);
+
+  if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  console.log(basket);
+};
+
+const increment = (id) => {
+  //let selectedItem = id;
+  //console.log(selectedItem.id);
+  let selectedID = id;
+  let search = basket.find((item) => item.id === selectedID);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedID,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  console.log(basket);
+};
+
+let update = () => {};
