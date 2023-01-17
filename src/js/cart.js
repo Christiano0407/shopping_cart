bcbcacca548ec7820b25cf1cfc63234f9dd37e9b
Christiano0407 <placebo0704@gmail.com>
2023-01-17 09:50:51 -0600

    add shopping cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,1 +1,9 @@
-//** === Cart Shopping */
+//** === === ============ Cart Shopping */
+const shoppingSection = document.querySelector(`#idShopping`);
+const cartShopping = document.querySelector(`#shoppingCart`);
+const label = document.getElementById(`label`);
+
+//** === === ============ Events && Code Shopping Cart */
+//**! === Basket Cart */
+let basket = JSON.parse(localStorage.getItem(`data`)) || [];
+console.log(basket);
