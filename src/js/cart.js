d34cc5be4b550929fbab7c67a3502db5f760ab08
Christiano0407 <placebo0704@gmail.com>
2023-01-17 12:15:50 -0600

    add shopping cart new buy

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,58 @@
 //** === === ============ Cart Shopping */
+
+//import { clothesShopData } from './data.js';
+
+const cardAmount = document.querySelector(`#idAmount`);
+const btnCheckout = document.getElementById(`#btnClear`);
+const btnClear = document.getElementById(`#btnClear`);
 const shoppingSection = document.querySelector(`#idShopping`);
 const cartShopping = document.querySelector(`#shoppingCart`);
 const label = document.getElementById(`label`);
 
+//console.log(clothesShopData);
+
 //** === === ============ Events && Code Shopping Cart */
 //**! === Basket Cart */
 let basket = JSON.parse(localStorage.getItem(`data`)) || [];
-console.log(basket);
+//console.log(basket);
+
+//** === Calculation  */
+const calculation = () => {
+  cardAmount.innerHTML = basket
+    .map((element) => element.item)
+    .reduce((x, y) => x + y, 0);
+  // console.log(basket.map((items) => items.item));
+};
+calculation();
+
+//**! === Cart Items */
+//== Create Custom Element AND add Product Cart ===
+const generateCartItems = () => {
+  if (basket.length !== 0) {
+    //console.log('Basket is not Empty');
+    return (cartShopping.innerHTML = basket
+      .map((item) => {
+        //console.log(item);
+        let = { id, item } = item;
+        let searchBuy = clothesShopData.find(
+          (product) => product.id === id || []
+        );
+        return `
+        <div class="cart-itemBuy">
+           <img src="${searchBuy.img}" alt="search-product" >
+        </div>
+     `;
+      })
+      .join());
+  } else {
+    //console.log('Basket, not product...');
+    cartShopping.innerHTML = ``;
+    label.innerHTML = ` 
+    <h2 class="label-text">Cart is Empty</h2>
+      <a href="/index.html" class="Label-btn">
+         <button class="btn-home">Back To Home</button>
+      </a>
+    `;
+  }
+};
+generateCartItems();
