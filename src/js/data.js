b53c99a52f2e313be745bed79bac7d4e5dbe8706
Christiano0407 <placebo0704@gmail.com>
2023-01-17 09:33:09 -0600

    add new shopping carts plus

--- src/js/data.js
+++ src/js/data.js
@@ -1,1 +1,40 @@
 //** === Data */
+
+const clothesShopData = [
+  {
+    id: `clothesOne`,
+    name: `T-Shirt Men`,
+    price: 500,
+    text: `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Veritatis vero sapiente dignissimos libero nihil commodi
+            explicabo.`,
+    img: `/src/img/shirtImg/shirt.jpg`,
+  },
+  {
+    id: `clothesTwo`,
+    name: `T-Shirt Edition Special`,
+    price: 350,
+    text: ` Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Veritatis vero sapiente dignissimos libero nihil commodi
+            explicabo.`,
+    img: `/src/img/shirtImg/shirt1.jpg`,
+  },
+  {
+    id: `clothesThird`,
+    name: `T-Shirt White Snow`,
+    price: 250,
+    text: `    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Veritatis vero sapiente dignissimos libero nihil commodi
+            explicabo.`,
+    img: `/src/img/shirtImg/shirt2.jpg`,
+  },
+  {
+    id: `clothesFour`,
+    name: `T-Shirt Special line`,
+    price: 450,
+    text: `    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Veritatis vero sapiente dignissimos libero nihil commodi
+            explicabo.`,
+    img: `/src/img/shirtImg/shirt4.jpg`,
+  },
+];
